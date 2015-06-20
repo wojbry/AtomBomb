47ca93cdb3061403aee2511ac3d9680c10a1f2bd
wojbry <brylinski.wojciech@gmail.com>
2015-06-20 12:01:36 +0200

    Dodalem drugi wykres energii w czasie

--- AtomBomb/src/pl/edu/pw/fizyka/pojava/atombomb/Chart.java
+++ AtomBomb/src/pl/edu/pw/fizyka/pojava/atombomb/Chart.java
@@ -3,6 +3,7 @@ package pl.edu.pw.fizyka.pojava.atombomb;
 import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -15,29 +16,49 @@ public class Chart extends JPanel implements Runnable {
 	
 	public Thread thread;
 	public Simulation sym;
+	XYSeries energy;
 	XYSeries totalEnergy;
-	JFreeChart chart;
+	JFreeChart chartTotalEnergy;
+	JFreeChart chartEnergy;
 	public Chart(Simulation sym1) {
 		totalEnergy=new XYSeries("Calkowita energia");
+		energy=new XYSeries("Energia");
 		sym=sym1;
-		XYSeriesCollection series = new XYSeriesCollection(totalEnergy);
-		chart = ChartFactory.createXYLineChart
+		XYSeriesCollection serieTotalEnergy = new XYSeriesCollection(totalEnergy);
+		XYSeriesCollection serieEnergy = new XYSeriesCollection(energy);
+		chartTotalEnergy = ChartFactory.createXYLineChart
                 ("E(t)",  // Title
                   "Czas [mikros]",           // X-Axis label
-                  "E [J]",           // Y-Axis label
-                  series,          // Dataset
+                  "E",           // Y-Axis label
+                  serieTotalEnergy,          // Dataset
                   PlotOrientation.VERTICAL,        //Plot orientation
-                  false,                //show legend
+                  true,                //show legend
                   true,                // Show tooltips
                   false               //url show
                  );
-		 ChartPanel chartPanel = new ChartPanel(chart);
+		chartEnergy = ChartFactory.createXYLineChart
+                ("E(t)",  // Title
+                  "Czas [mikros]",           // X-Axis label
+                  "E",           // Y-Axis label
+                  serieEnergy,          // Dataset
+                  PlotOrientation.VERTICAL,        //Plot orientation
+                  true,                //show legend
+                  true,                // Show tooltips
+                  false               //url show
+                 );
+		 ChartPanel chartPanelTotalEnergy = new ChartPanel(chartTotalEnergy);
+		 ChartPanel chartPanelEnergy = new ChartPanel(chartEnergy);
 		 setLayout(new BorderLayout());
-		 this.add(chartPanel, BorderLayout.CENTER);
+		 
+		 JTabbedPane tabbedPane = new JTabbedPane();
+		 tabbedPane.addTab("Total Energy(t)", null, chartPanelTotalEnergy, null);
+		 tabbedPane.addTab("Energy(t)", null, chartPanelEnergy, null);
+		 this.add(tabbedPane, BorderLayout.CENTER);
 	}
 	
 	public void clear(){
 		totalEnergy.clear();
+		energy.clear();
 	}
 	
 	public void start() {
@@ -57,13 +78,15 @@ public class Chart extends JPanel implements Runnable {
 	@Override
 	public void run() {
 		sym.placeAtoms();
-		while(thread!=null){			
+		while(thread!=null){	
+			sym.energy=0;
 			sym.iterateAtoms();
 			sym.iterateNeutrons();
 			sym.neutrons.addAll(sym.neutrons1);
 			sym.neutrons1.clear();
 			sym.t+=sym.dt;
-			totalEnergy.add(sym.t*1E6, sym.totalEnergy);
+			totalEnergy.add(sym.t*1E12, sym.totalEnergy);
+			energy.add(sym.t*1E12, sym.energy);
 			if(sym.numberOfFissioned>=0.7*sym.atoms.size())
 				stop();
 			try {
